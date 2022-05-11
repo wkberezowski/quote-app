fa0df222358a3c871b4961ae5c41cab918fc43c1
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-11 13:55:28 +0200

    added sorting quotes and flexible routing

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,8 +1,8 @@
 import { Fragment } from 'react';
-import { useParams, Route } from 'react-router-dom';
+import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
 
-import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
 
 const DUMMY_QUOTES = [
   { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
@@ -10,20 +10,26 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.qouteId);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
   if (!quote) {
-    return <p>No quote found</p>;
+    return <p>No quote found!</p>;
   }
 
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={match.path} exact>
+        <div className='centered'>
+          <Link className='btn--flat' to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
