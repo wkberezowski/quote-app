2a09ad8b33e080eec00c8410afd8a29df78988bb
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-11 12:41:45 +0200

    added redirects

---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,13 @@
+import { useParams } from 'react-router-dom';
+
+const QuoteDetail = () => {
+  const params = useParams();
+  return (
+    <>
+      <h1> Quotes Detail Page</h1>
+      <p>{params.quoteId}</p>
+    </>
+  );
+};
+
+export default QuoteDetail;
